dc91d40e5c7a5844019465849a347a08fa5003ab
Franklin Floresca <franklin.jhf@gmail.com>
2017-07-18 12:18:46 -0500

    everything is completed

--- controller.js
+++ controller.js
@@ -1,5 +1,8 @@
-angular.module('userProfiles').controller('MainController', function($scope){
+angular.module('userProfiles').controller('MainController', function($scope, mainSrvc){
     
     $scope.thisAppIsBroken = "This angular app is working";
+
+    $scope.users = mainSrvc.getUsers();
    
+    $scope.toggleFavorite = mainSrvc.toggleFavorite;
 })
